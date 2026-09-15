6d58519711f6c893dc9939c5efaa3e4d6ca0fc4b
Perin0 <125603399+Perin0@users.noreply.github.com>
2026-09-15 00:42:52 +0000

    Criação e resolução do Exercício 1 e 2

--- Corte-Agil/src/app/app-routing-module.ts
+++ Corte-Agil/src/app/app-routing-module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: 'livros',
     loadChildren: () => import('./livros/livros-module').then(a => a.LivrosModule)
+  },
+  {
+    path: 'lista-de-exercicios',
+    loadChildren: () => import('./lista-de-exercicios/lista-de-exercicios-module').then(a => a.ListaDeExerciciosModule)
   }
 ];
 
