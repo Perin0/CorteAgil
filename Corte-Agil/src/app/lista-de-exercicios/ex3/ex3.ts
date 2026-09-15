53f02b6e286c8cb43732b76f9b20327b24d5932b
Perin0 <125603399+Perin0@users.noreply.github.com>
2026-09-15 00:56:37 +0000

    Criação e resolução dos exercícios 3 e 4

---
+++ Corte-Agil/src/app/lista-de-exercicios/ex3/ex3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex3',
+  standalone: false,
+  templateUrl: './ex3.html',
+  styleUrl: './ex3.scss',
+})
+export class Ex3 {
+  imageURL = "https://media1.tenor.com/m/x8v1oNUOmg4AAAAd/rickroll-roll.gif"
+}
